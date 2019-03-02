0e389197a56306ad40db238caee56e8cec147c8c
Shreyas Raj Singh <srs231198@gmail.com>
2019-03-02 14:02:10 -0600

    Got it working!!

--- main.cpp
+++ main.cpp
@@ -6,40 +6,49 @@
 
 using namespace std;
 
-const int col = 4;
+
 
 //a function to get the input from matrix.txt
-void FileInput(int *, fstream &);
+void FileInput(double *, fstream &, int );
 
 //a function to count the number of players;
-//int numOfPlayers(fstream &);
+int numOfPlayers(fstream &);
 
 int main(){
 
     fstream inputfile;
-    int *matrix;
-    //int num_players = numOfPlayers(inputfile);
-    matrix = new int[16];
-//    int num_equations = 0;
-    //inputfile.open("commands.txt", ios :: in);
+    double *matrix;
+
+    int num_equations = 0;
+    num_equations = numOfPlayers(inputfile);
+    int sizeOfArray = num_equations*4;
+
+    matrix = new double[sizeOfArray]();
+
     //call the function and store the value of the num of rows in num_equations
-    FileInput(matrix, inputfile);
+    FileInput(matrix, inputfile, num_equations);
+
     inputfile.close();
 }
 
-//
-//int numOfPlayers(fstream &inputfile){
-//    string player;
-//    int playerCount = 0;
-//    while(!inputfile.eof()){
-//        getline(inputfile,player,'\n');
-//        playerCount++;
-//    }
-//    int t = inputfile.tellp();
-//
-//
-//    return playerCount;
-//}
+
+int numOfPlayers(fstream &inputfile){
+    string player;
+    int playerCount = 0;
+    inputfile.open("commands.txt", ios :: in);
+    int t = (int)inputfile.tellp();
+
+    while(!inputfile.eof()){
+        getline(inputfile,player);
+        playerCount++;
+    }
+
+ //   int t = inputfile.tellp();
+    inputfile.seekp(0, ios::beg);
+
+    inputfile.close();
+    return playerCount;
+}
 
 
 //Funtion below will open the file given and parse through the equation in the file
@@ -50,15 +59,75 @@ int main(){
 //                    (1 , 1, 1, 11)]
 //in matrix[][]
 
-void FileInput(int *matrix, fstream &inputfile) {
-    int var = 0;
+void FileInput(double *matrix, fstream &inputfile, int numEQ) {
+    int var = 0, sign = 1;
     char ch;
-    int *ptm;
+    double *ptm;
     ptm = matrix;
 
-    while(!inputfile.eof()){
+    inputfile.open("commands.txt", ios::in);
+    int t = (int) inputfile.tellp();
+
+    for(int i = 0; i < numEQ ; i++) {
+
+        int col = 4;
+
+        //while there are less than 4 cols
+        //the loop will also terminate if we reach a newline or constant
+        while (col > 0) {
+            //check for the character pointed at by the file pointer
+            // and store it in c
+            int c = inputfile.peek();
+
+            //check for newline
+            if(c == 10){
+                break;
+            }
+
+            //check if the val is a digit
+            if (isdigit(c)) {
+                inputfile >> var;
+            }
+            else{
+                var = 1;
+            }
+
+            //check for character
+            c = inputfile.peek();
+            if (isalpha(c)) {
+
+                inputfile >> ch;
+
+                if (c == 120) {
+                    *ptm = var*sign;
+                }
+                else if (c == 121) {
+                    ptm++;
+                    *ptm = var*sign;
+                    ptm--;
+                }
+                else if (c == 122) {
+                    ptm += 2;
+                    *ptm = var*sign;
+                    ptm -= 2;
+                }
+            }
+
+            //check for operator
+            c = inputfile.peek();
+            if(c == 45){
+                sign = -1;
+            }
+            else{
+                sign = 1;
+            }
+
+            //decrement the coloum
+            col--;
+            //move pointer to the next set of positions
+            ptm += 4;
+        }
 
     }
-
-
+    inputfile.close();
 }
